c0f93603906a0ca565f1f7019e272d5ba9a56552
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-05-06 03:14:59 -0700

    updated..

--- index.js
+++ index.js
@@ -94,6 +94,16 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
+// Enable CORS (see https://enable-cors.org/server_expressjs.html)
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 
 /*==================
   Start Express Server
